4a9c7966099022b68a2bf5f8c9665c1314f70ab0
Daniel Antonio Lopez Delgado <danielopez07@gmail.com>
2020-09-20 20:17:11 -0300

    Adding plugin header

---
+++ wp-anchor-tags-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP anchor tags plugin
+Plugin URI: https://github.com/danielopez07/wp-anchor-tags-plugin
+Description: Enable support for anchor tags for all blocks in the WordPress editor
+Author: Daniel Lopez
+Author URI: https://danielopez07.github.io/danielopez07/
+Text Domain: wp-anchor-tags-plugin
+Domain Path: /lang
+*/
+
